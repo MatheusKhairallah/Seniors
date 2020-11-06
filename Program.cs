519bd1cb770a11ea181ae569e1651675e85a448b
Matheus <playermaster10like@gmail.com>
2020-11-05 22:31:00 -0300

    programa que busca funcionarios mais velhos

--- Program.cs
+++ Program.cs
@@ -12,19 +12,17 @@ namespace Seniors
 
             using (var db= new employeesContext())
             {
-                var masculino= db.Employees.Where(m=> m.Gender=="M");
-                foreach (var funciM in masculino)
-                {
-                    string sobrenome= funciM.LastName.ToUpper();
-                    Console.WriteLine($"{sobrenome}, {funciM.FirstName}. Gênero: {funciM.Gender}");
-                }
-
-                var feminino= db.Employees.Where(f=> f.Gender=="F");
-                foreach (var funciF in feminino)
-                {
-                    string sobrenome= funciF.LastName.ToUpper();
-                    Console.WriteLine($"{sobrenome}, {funciF.FirstName}. Gênero: {funciF.Gender}");
-                }
+                var masculino= db.Employees.Where(m=> m.Gender=="M").First();
+                string sobrenomeM= masculino.LastName.ToUpper();
+                string nomeM= masculino.FirstName;
+                string generoM= masculino.Gender;
+                Console.WriteLine($"{sobrenomeM}, {nomeM}. Gênero: {generoM}");
+                  
+                var feminino= db.Employees.Where(f=> f.Gender=="F").First();
+                string sobrenomeF= feminino.LastName.ToUpper();
+                string nomeF= feminino.FirstName;
+                string generoF= feminino.Gender;
+                Console.WriteLine($"{sobrenomeF}, {nomeF}. Gênero: {generoF}");
             }
         }
     }
